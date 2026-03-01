75670fab299c5dd7dd9a216fc1b4101eb1b90353
fabiosouzacontador <fabiocontadoriema@gmail.com>
2026-03-01 16:05:40 -0300

    Create routes package initialization

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,21 +1,7 @@
-from .auth import router as auth_router
-from .users import router as users_router
-from .contas import router as contas_router
-from .lancamentos import router as lancamentos_router
-from .turmas import router as turmas_router
-from .escolas import router as escolas_router
-from .atividades import router as atividades_router
-from .relatorios import router as relatorios_router
-from .ia_tutor import router as ia_tutor_router
+# routes package initialization
 
-__all__ = [
-    "auth_router",
-    "users_router",
-    "contas_router",
-    "lancamentos_router",
-    "turmas_router",
-    "escolas_router",
-    "atividades_router",
-    "relatorios_router",
-    "ia_tutor_router",
-]
+"""
+This package is responsible for defining the routes for the Guriata Contabilidade SaaS application.
+
+You can define your application's routes in this file.
+"""
